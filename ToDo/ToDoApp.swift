6b7c755fbedf88229a44438499e348b15106eb47
Adarsh Raghav <aadarshraghav@gmail.com>
2021-05-04 19:12:12 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Adarsh Raghav on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
